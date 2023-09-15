dbdd341790b0f900d80374689ad676fe11ce4c9a
Sainy Jee <sainyjee@MacBook-Air-Yaroslav.local>
2023-09-15 14:27:14 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const navButton = document.querySelector('.nav-button');
+const nav = document.querySelector('.nav');
+const navImg = document.querySelector('.nav-button-img');
+
+
+navButton.addEventListener('click', () => {
+    nav.classList.toggle('open');
+    if (navButton.classList.toggle('open')) {
+        navImg.src = './img/nav-close.svg';
+        
+    } else {
+        navImg.src = './img/nav-open.svg';
+    }
+    
+} );
